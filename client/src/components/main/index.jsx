37cc3b4f55d798c956490dd95b4344b1fdae5ee1
Johnydavid <dropin2johny@gmail.com>
2023-06-08 21:49:21 +0530

    c

--- client/src/components/main/index.jsx
+++ client/src/components/main/index.jsx
@@ -7,19 +7,19 @@ const Main = () => {
     window.location.reload();
   };
 
-  const myDropDown = () => {
-    document.getElementById("myDropdown").classList.toggle("show");
-  };
+  // const myDropDown = () => {
+  //   document.getElementById("myDropdown").classList.toggle("show");
+  // };
 
   // Close the dropdown if the user clicks outside of it
-  window.onclick = function (e) {
-    if (!e.target.matches(".dropbtn")) {
-      var myDropdown = document.getElementById("myDropdown");
-      if (myDropdown.classList.contains("show")) {
-        myDropdown.classList.remove("show");
-      }
-    }
-  };
+  // window.onclick = function (e) {
+  //   if (!e.target.matches(".dropbtn")) {
+  //     var myDropdown = document.getElementById("myDropdown");
+  //     if (myDropdown.classList.contains("show")) {
+  //       myDropdown.classList.remove("show");
+  //     }
+  //   }
+  // };
   const value = window.localStorage.getItem("displayName");
   return (
     <div className={styles.main_container}>
@@ -27,19 +27,11 @@ const Main = () => {
         <h1>GUVI</h1>
         <h3>Welcome {value}</h3>
         <button className={styles.white_btn} onClick={handleLogout}>
-          {value}
+     
           Logout
-        </button>
-        <div className={styles.dropdown}>
-          <button className={styles.dropbtn} onClick={myDropDown}>
-            {value}
           </button>
-          <div className={styles.dropdownContent} id="myDropdown">
-            <button className={styles.white_btn} onClick={handleLogout}>
-              Logout
-            </button>
-          </div>
-        </div>
+        
+      
       </nav>
     </div>
   );

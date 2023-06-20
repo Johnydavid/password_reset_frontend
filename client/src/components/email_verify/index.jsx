1800d46e51d55b2289e568ad873927fd0caa6f43
Johnydavid <dropin2johny@gmail.com>
2023-06-20 21:24:10 +0530

    c

--- client/src/components/email_verify/index.jsx
+++ client/src/components/email_verify/index.jsx
@@ -1,47 +1,47 @@
 import { useEffect, useState } from "react";
+import React, { Fragment } from "react";
 import { useParams, Link } from "react-router-dom";
+
 import axios from "axios";
-// import success from "../../images/success.png";
+
 import styles from "./styles.module.css";
-// import { Fragment } from "react/cjs/react.production.min";
 
 const EmailVerify = () => {
-	const [validUrl, setValidUrl] = useState(false);
-	const param = useParams();
+  const [validUrl, setValidUrl] = useState(false);
+  let param = useParams();
 
-	useEffect(() => {
-		const verifyEmailUrl = async () => {
-			try {
-				const url = `http://localhost:8080/api/users/${param.id}/verify/${param.token}`;
-				const { data } = await axios.get(url);
-				console.log(data);
-				setValidUrl(true);
-			} catch (error) {
-				console.log(error);
-				setValidUrl(false);
-			}
-		};
-		verifyEmailUrl();
-	}, [param]);
+  useEffect(() => {
+    const verifyEmailUrl = () => {
+      const url = `http://localhost:8080/api/users/${param.id}/verify/${param.token}`;
+      console.log(param.id);
+      axios
+        .get(url)
+        .then((res) => {
+          setValidUrl(true);
+        })
+        .catch((err) => {
+          console.log(err);
+          setValidUrl(false);
+        });
+    };
+    verifyEmailUrl();
+  }, [param]);
 
-	return (
-        <div>
-		{/* <Fragment> */}
-			{validUrl ? (
-				<div className={styles.container}>
-					{/* <img src={success} alt="success_img" className={styles.success_img} /> */}
-					<h1>Email verified successfully</h1>
-					<Link to="/login">
-						<button className={styles.green_btn}>Login</button>
-					</Link>
-				</div>
-			) : (
-				<h1>404 Not Found</h1>
-			)}
-        
-		{/* </Fragment> */}
+  return (
+    <Fragment>
+      {validUrl ? (
+        <div className={styles.container}>
+          <h1>Email verified successfully</h1>
+
+          <Link to="/login">
+            <button className={styles.green_btn}>Login</button>
+          </Link>
         </div>
-	);
+      ) : (
+        <h1>404 Not Found</h1>
+      )}
+    </Fragment>
+  );
 };
 
 export default EmailVerify;
--- client/src/components/main/index.jsx
+++ client/src/components/main/index.jsx
@@ -7,19 +7,6 @@ const Main = () => {
     window.location.reload();
   };
 
-  // const myDropDown = () => {
-  //   document.getElementById("myDropdown").classList.toggle("show");
-  // };
-
-  // Close the dropdown if the user clicks outside of it
-  // window.onclick = function (e) {
-  //   if (!e.target.matches(".dropbtn")) {
-  //     var myDropdown = document.getElementById("myDropdown");
-  //     if (myDropdown.classList.contains("show")) {
-  //       myDropdown.classList.remove("show");
-  //     }
-  //   }
-  // };
   const value = window.localStorage.getItem("displayName");
   return (
     <div className={styles.main_container}>
@@ -27,11 +14,8 @@ const Main = () => {
         <h1>GUVI</h1>
         <h3>Welcome {value}</h3>
         <button className={styles.white_btn} onClick={handleLogout}>
-     
           Logout
-          </button>
-        
-      
+        </button>
       </nav>
     </div>
   );

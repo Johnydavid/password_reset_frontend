542cccc9214d933e22c05aa32dc29dcc86d98edb
Johnydavid <dropin2johny@gmail.com>
2023-06-23 12:20:21 +0530

    c

--- client/src/App.js
+++ client/src/App.js
@@ -10,17 +10,19 @@ function App() {
   const user = localStorage.getItem("token");
 
   return (
+
+
     <BrowserRouter>
-      <Routes>
-        {user && <Route path="/" exact element={<Main />} />}
-        <Route path="/signup" exact element={<Signup />} />
-        <Route path="/login" exact element={<Login />} />
-        <Route path="/" element={<Navigate replace to="/login" />} />
-        <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
-        <Route path="/forgot-password" element={<ForgotPassword />} />
-        <Route path="/password-reset/:id/:token" element={<PasswordReset />} />
-      </Routes>
-    </BrowserRouter>
+    <Routes>
+      {user && <Route path="/" exact element={<Main />} />}
+      <Route path="/signup" exact element={<Signup />} />
+      <Route path="/login" exact element={<Login />} />
+       <Route path="/" element={<Navigate replace to="/login" />} />
+     <Route path="/users/:id/verify/:token" element={<EmailVerify />} />
+     <Route path="/forgot-password" element={<ForgotPassword />} />
+     <Route path="/password-reset/:id/:token" element={<PasswordReset />} />
+  </Routes>
+   </BrowserRouter>
   );
 }
 
